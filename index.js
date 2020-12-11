846fc18844ad047c5407d0a2de339d9f18fa1be2
elChupaCambra <claudio.cambra@gmail.com>
2020-12-12 00:28:17 +0100

    Added index.html and styling

---
+++ index.js
@@ -0,0 +1,16 @@
+function linkSwitcher(elementid) {
+	switch(elementid) {
+		case "bourdain":
+			window.open
+		case "corgi":
+		case "fpga":
+		case "portfolio":
+		case "feedback":
+		case "clock":
+		case "drum":
+		case "quote":
+		case "previewer":
+		case "calculator":
+
+
+}
