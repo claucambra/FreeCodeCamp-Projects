8d8be88eeac4070ef2ef389ed8b1fbb9cb604802
elChupaCambra <claudio.cambra@gmail.com>
2020-12-12 10:33:33 +0100

    Added interactivity to html. Rearranged files.

---
+++ index-files/index.js
@@ -0,0 +1,26 @@
+function linkSwitcher(elementid) {
+	switch(elementid) {
+		//Responsive Web Design
+		case "bourdain":
+			window.open("elchupacambra.github.io/FreeCodeCamp-Projects/Responsive\ Web\ Design/Anthony\ Bourdain\ Tribute/anthony-bourdain.html");
+			break;
+		case "corgi":
+			window.open("elchupacambra.github.io/FreeCodeCamp-Projects/Responsive\ Web\ Design/Corgi-Express-Shop/Corgi-Express-Shop.html");
+			break;
+		case "fpga":
+			window.open("elchupacambra.github.io/FreeCodeCamp-Projects/Responsive\ Web\ Design/FPGA-Intro-Doc-Demo/FPGA-Doc.html");
+			break;
+		case "portfolio":
+			window.open("elchupacambra.github.io/FreeCodeCamp-Projects/Responsive\ Web\ Design/Personal Portfolio Site/portfolio.html");
+			break;
+		case "feedback":
+			window.open("elchupacambra.github.io/FreeCodeCamp-Projects/Responsive\ Web\ Designn/Site\ Feedback\ Form/Site-feedback-form.html");
+			break;
+		//Front End Libraries
+		case "clock":
+		case "drum":
+		case "quote":
+		case "previewer":
+		case "calculator":
+	}
+}
