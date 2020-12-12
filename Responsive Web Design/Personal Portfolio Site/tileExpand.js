89d4370d00d1525a2f4a3a7e7e206b7f5510ee4e
elChupaCambra <claudio.cambra@gmail.com>
2020-12-12 17:49:48 +0100

    Fixed Portfolio tiles not opening on non-Gecko browsers

--- Responsive Web Design/Personal Portfolio Site/tileExpand.js
+++ Responsive Web Design/Personal Portfolio Site/tileExpand.js
@@ -1,31 +1,30 @@
 function tileExpand(target) {
-  console.log(target.id);
-  if (target.style.gridArea != "auto") {
-    target.style.gridArea = "auto";
-    switch (target.id) {
-      case "codethink-tile":
-        document.getElementById("codethink-text").style.display = "none";
-        break;
-      case "lm-tile":
-        document.getElementById("lm-text").style.display = "none";
-        break;
-      case "pm-tile":
-        document.getElementById("pm-text").style.display = "none";
-        break;
-    }
-  } else {
-    target.style.gridArea = "auto / auto / auto / span 2";
-    switch (target.id) {
-      case "codethink-tile":
-        document.getElementById("codethink-text").style.display = "block";
-        break;
-      case "lm-tile":
-        document.getElementById("lm-text").style.display = "block";
-        break;
-      case "pm-tile":
-        document.getElementById("pm-text").style.display = "block";
-        break;        
-    }
-  }
+	console.log(target.style.gridArea);
+	if (target.style.gridArea != "auto / auto / auto / span 2") {
+		target.style.gridArea = "auto / auto / auto / span 2";
+		switch (target.id) {
+		case "codethink-tile":
+			document.getElementById("codethink-text").style.display = "block";
+			break;
+		case "lm-tile":
+			document.getElementById("lm-text").style.display = "block";
+			break;
+		case "pm-tile":
+			document.getElementById("pm-text").style.display = "block";
+			break;     
+		}
+	} else {
+		target.style.gridArea = "auto";
+		switch (target.id) {
+		case "codethink-tile":
+			document.getElementById("codethink-text").style.display = "none";
+			break;
+		case "lm-tile":
+			document.getElementById("lm-text").style.display = "none";
+			break;
+		case "pm-tile":
+			document.getElementById("pm-text").style.display = "none";
+			break;
+		}
+	}
 }
-
